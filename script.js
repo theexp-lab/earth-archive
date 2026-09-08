63609f722e1e11397b5d948d807d608fe88d252c
theexp-lab <louann.desia@gmail.com>
2026-09-09 00:52:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const beginButton = document.getElementById("begin-button");
 
+const continueButton = document.getElementById("continue-button");
+
 const landingScreen = document.getElementById("landing-screen");
 
 const missionScreen = document.getElementById("mission-screen");
+
+const physicalScreen = document.getElementById("physical-screen");
 
 
 beginButton.addEventListener("click", function () {
@@ -10,6 +14,17 @@ beginButton.addEventListener("click", function () {
   landingScreen.classList.add("hidden");
 
   missionScreen.classList.remove("hidden");
+
+  window.scrollTo(0, 0);
+
+});
+
+
+continueButton.addEventListener("click", function () {
+
+  missionScreen.classList.add("hidden");
+
+  physicalScreen.classList.remove("hidden");
 
   window.scrollTo(0, 0);
 
